3fcac0a0482b540e4ab260b6f31b2f26f0d6783b
Aaron Tully <aarontully0@gmail.com>
2025-03-20 10:21:24 +1100

    fixed sig layout

--- src/runtime/Js/autorunshared.js
+++ src/runtime/Js/autorunshared.js
@@ -181,8 +181,10 @@ function get_template_A_info(user_info) {
   str +=   is_valid_data(user_info.phone) ? `<div>${user_info.phone}</div>` : "";
   str +=   "<div>Head Office: 42-44 Victoria Street, McMahons Point, NSW 2060</div>"
   str +=   "<div style='margin-bottom: 10px;'><a href='https://www.endemolshine.com.au' target='_blank'>Endemol Shine Australia</a></div>";
-  str +=   "<img height='90' width='380' style='margin-bottom: 8px;' src='https://cdn.endemolshine.com.au/assets/img/esa_email_signature_rotating.gif' alt='Endemol Shine Australia' />";
-  str +=   "<img height='25' width='230' src='https://cdn.endemolshine.com.au/assets/img/esa-logo-wide.jpg' alt='Logo' />";
+  str +=   "<div style='display: flex; flex-direction: column;'>";
+  str +=    "<img height='90' width='380' style='margin-bottom: 8px;' src='https://cdn.endemolshine.com.au/assets/img/esa_email_signature_rotating.gif' alt='Endemol Shine Australia' />";
+  str +=    "<img height='25' width='230' src='https://cdn.endemolshine.com.au/assets/img/esa-logo-wide.jpg' alt='Logo' />";
+  str +=   "</div>";
   str +=   "<div style='margin-top: 10px; font-size: xx-small; font-style: italic;'>This electronic mail, including any attachments, is intended for the addressee only and may contain information that is either confidential or subject to legal professional privilege. Unauthorised reproduction, use or disclosure of the contents of this mail is prohibited. If you have received this mail in error, please delete it from your system immediately and notify Endemol Shine Australia by contacting us at <a href='https://www.endemolshine.com.au'>www.endemolshine.com.au</a>.</div>";
   str += "</div>";
 
