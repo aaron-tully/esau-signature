aa65d65f473c8e28b416825458aa27b7af0ceee9
Aaron Tully <aarontully0@gmail.com>
2025-03-20 09:36:13 +1100

    updated images in sig

--- src/runtime/Js/autorunshared.js
+++ src/runtime/Js/autorunshared.js
@@ -170,19 +170,20 @@ function get_template_A_info(user_info) {
   const logoFileName = "sample-logo.png";
   let str = "";
 
-  str += "<div>";
+  str += "<div style='font-family: 'Aptos', Verdana, sans-serif; font-size: 12px;'>";
+  str +=   "<br>";
   str +=   is_valid_data(user_info.greeting) ? `<div style='margin-bottom: 10px;'>${user_info.greeting},</div>` : "";
   str +=   "<div><strong style='color: #F6688A;'>" + user_info.name + "</strong>";
   str +=   is_valid_data(user_info.pronoun) ? ` ${user_info.pronoun}` : "";
   str +=   "</div>";
   str +=   is_valid_data(user_info.job) ? `<div>${user_info.job}</div>` : "";
   str +=   "<br>";
-  str +=   "<div>" + user_info.email + "</div>";
   str +=   is_valid_data(user_info.phone) ? `<div>${user_info.phone}</div>` : "";
-  str +=   is_valid_data(user_info.department) ? `<div>${user_info.department}</div>` : "";
-  str +=   is_valid_data(user_info.location) ? `<div>${user_info.location}</div>` : "";
+  str +=   "<div>Head Office: 42-44 Victoria Street, McMahons Point, NSW 2060</div>"
   str +=   "<div style='margin-bottom: 10px;'><a href='https://www.endemolshine.com.au' target='_blank'>Endemol Shine Australia</a></div>";
-  str +=   "<img src='https://aarontully.github.io/ESA-Signature-Addin/assets/sig_image.png' alt='Logo' />";
+  str +=   "<img height='90' width='380' style='margin-bottom: 8px;' src='https://cdn.endemolshine.com.au/assets/img/esa_email_signature_rotating.gif' alt='Endemol Shine Australia' />";
+  str +=   "<img height='25' width='230' src='https://cdn.endemolshine.com.au/assets/img/esa-logo-wide.jpg' alt='Logo' />";
+  str +=   "div style='margin-top: 10px; font-size: xx-small; font-style: italic;'>This electronic mail, including any attachments, is intended for the addressee only and may contain information that is either confidential or subject to legal professional privilege. Unauthorised reproduction, use or disclosure of the contents of this mail is prohibited. If you have received this mail in error, please delete it from your system immediately and notify Endemol Shine Australia by contacting us at <a href='https://www.endemolshine.com.au'>www.endemolshine.com.au</a>.</div>";
   str += "</div>";
 
   // return object with signature HTML, logo image base64 string, and filename to reference it with.
@@ -205,7 +206,8 @@ function get_template_A_info(user_info) {
  */
 function get_template_B_info(user_info) {
   let str = "";
-  str += "<div>";
+  str += "<div style='font-family: 'Aptos', Verdana, sans-serif; font-size: 12px;'>";
+  str +=   "<br>";
   str +=   is_valid_data(user_info.greeting) ? `<div style='margin-bottom: 10px;'>${user_info.greeting},</div>` : "";
   str +=   "<div><strong style='color: #F6688A;'>" + user_info.name + "</strong>";
   str +=   is_valid_data(user_info.pronoun) ? ` ${user_info.pronoun}` : "";
